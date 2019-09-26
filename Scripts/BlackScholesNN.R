471950ad51e9254fc86c0ffd9aba5e2f793a0f27
Magnus Birkemose <mnorda@student.aau.dk>
2019-09-26 11:30:26 +0200

    BlackScholesNN:: for server

--- Scripts/BlackScholesNN.R
+++ Scripts/BlackScholesNN.R
@@ -4,14 +4,40 @@ library(dplyr)
 library(neuralnet)
 
 ### Inlæs data --------------------------------------------------------------------------
-Data <- read.csv("./Data/BlackScholesData.csv")
+data <- read.csv("./Data/BlackScholesData.csv.gz")
 
 set.seed(1)
-trainInd <- sample(nrow(Data), nrow(Data)*0.001)
-trainData <- Data[trainInd,]
-testData <- Data[-trainInd,]
+indTrain <- sample(nrow(data), nrow(data)*0.8)
+dataTrain <- data[indTrain,]
+dataTest <- data[-indTrain,]
 
 ### Træn NN -----------------------------------------------------------------------------
-nnBlackScholes <- neuralnet(CHat~S0 + K + r + MT + sigma, data = trainData, 
+startTime <- Sys.time()
+
+nnBlackScholes <- neuralnet(C ~ S0 + K + r + MT + sigma, data = dataTrain, 
                             hidden = c(30,30),
-                            stepmax = )
+                            threshold = 0.01,
+                            stepmax = 1e+6,
+                            learningrate.factor = list(minus = 0.5,
+                                                       plus = 1.2),
+                            lifesign = "minimal")
+
+endTime <- Sys.time()
+trainTime <- endTime - startTime
+
+### Prædikterer -------------------------------------------------------------------------
+predictTrain <- compute(nnBlackScholes, dataTrain)
+predictTest <- compute(nnBlackScholes, dataTest)
+
+
+dataTrain <- dataTrain %>% 
+  mutate(predictedNN = predictTrain[["net.result"]])
+dataTest <- dataTest %>% 
+  mutate(predictedNN = predictTest[["net.result"]])
+
+
+### Gemmer model ------------------------------------------------------------------------
+save(nnBlackScholes, trainTime,
+     file = "./Workspaces/BlackScholesNnWorkspace.Rdata")
+
+quit(save = "no")
